ef44b8dd84876f51c662e2184f709cde98e0ff34
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-02-13 13:30:54 -0800

    created shared Button component and updated review form with style, functionality and validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Text must be atleast 10 characters')
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
     setText(e.target.value)
   }
+
   return (
     <Card>
       <form>
@@ -19,8 +33,11 @@ function FeedbackForm() {
             placeholder='Write a review'
             value={text}
           />
-          <button type='submit'>Send</button>
+          <Button type='submit' version='secondary' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
