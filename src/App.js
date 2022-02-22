3b2beb565de92a3cced7b5bd7d9a7d976c1d9fa9
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-02-21 22:17:19 -0800

    added about icon that links to about page and change href links to Link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -28,19 +29,20 @@ function App() {
                 <Header />
                 <div className='container'>
                     <Routes>
-                        <Route exact path='/' 
-                        element={
-                            <>
-                                <FeedbackForm handleAdd={addFeedback} />
-                                <FeedbackStats feedback={feedback} />
-                                <FeedbackList feedback={feedback}
-                                    handleDelete={deleteFeedback} />
-                            </>
-                        }
+                        <Route exact path='/'
+                            element={
+                                <>
+                                    <FeedbackForm handleAdd={addFeedback} />
+                                    <FeedbackStats feedback={feedback} />
+                                    <FeedbackList feedback={feedback}
+                                        handleDelete={deleteFeedback} />
+                                </>
+                            }
                         ></Route>
 
                         <Route path='/about' element={<AboutPage />} />
                     </Routes>
+                    <AboutIconLink />
                 </div>
             </Router>
         </>
