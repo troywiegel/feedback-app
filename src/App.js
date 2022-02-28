f40c4aa4514483e7306172cfe13175d94a6ceef0
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-02-27 17:12:13 -0800

    creating context and warpping app.js content in it

--- src/App.js
+++ src/App.js
@@ -1,52 +1,57 @@
-import { v4 as uuidv4 } from 'uuid'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
-import FeedbackStats from './components/FeedbackStats'
-import FeedbackForm from './components/FeedbackForm'
-import AboutPage from './pages/AboutPage'
-import AboutIconLink from './components/AboutIconLink'
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
+  };
 
-    const deleteFeedback = (id) => {
-        if (window.confirm('Are you sure you want to delete?')) {
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
 
-    return (
-        <>
-            <Router>
-                <Header />
-                <div className='container'>
-                    <Routes>
-                        <Route exact path='/'
-                            element={
-                                <>
-                                    <FeedbackForm handleAdd={addFeedback} />
-                                    <FeedbackStats feedback={feedback} />
-                                    <FeedbackList feedback={feedback}
-                                        handleDelete={deleteFeedback} />
-                                </>
-                            }
-                        ></Route>
-
-                        <Route path='/about' element={<AboutPage />} />
-                    </Routes>
-                    <AboutIconLink />
-                </div>
-            </Router>
-        </>
-    )
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  );
 }
 
-export default App
+export default App;
